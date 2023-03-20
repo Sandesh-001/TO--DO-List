f2e514e4a57628f03982b13c92d6993ad6e945dc
Sandesh-001 <sp9900725511@gmail.com>
2023-03-21 02:41:59 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, {  useState } from 'react'
+import "./Components/App.css"
+import Form from './Components/Form'
+
+import Header from './Components/Header'
+import TodoList from './Components/TodoList'
+
+const App = () => {
+
+    const[input,setInput]=useState("");
+    const[todos,setTodos]=useState([])
+    const[editTodo,setEditTodo]=useState(null)
+    
+
+
+
+  return (
+    <div className='container'>
+    <div className="app-wrapper">
+        <div>
+            <Header/>
+        </div>
+        <div>
+            <Form
+             input={input}
+             setInput={setInput}
+             todos={todos}
+             setTodos={setTodos}
+             editTodo={editTodo}
+             setEditTodo={setEditTodo}/>
+        </div>
+        <div>
+            <TodoList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo}/>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default App
